6bcf20390110f9cc79bcfdea0f4adea3bd7f0a33
Alexey Korchevskiy <mitallast@gmail.com>
2015-10-27 13:39:54 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Please
+======
+
+Simple console utility to combine miltiple package managers at one.
+
+How to build
+============
+
+```sh
+go build
+```
+
+Example of usage
+================
+
+$ please install python
+[brew install python]
+Warning: python-2.7.10_2 already installed
+
+Credits
+=======
+
+Alexey Korchevsky @mitallast
+@samosad
---
+++ please.go
@@ -0,0 +1,16 @@
+package main
+
+import "os"
+import "fmt"
+import "os/exec"
+
+func main() {
+    brew := append([]string{"brew"}, os.Args[1:]...)
+    fmt.Println(brew)
+    out, err := exec.Command("brew", os.Args[1:]...).CombinedOutput()
+    if err != nil {
+        fmt.Println("error occured")
+        fmt.Printf("%s", err)
+    }
+    fmt.Printf("%s", out)
+}
