cfabff50abca141eb5db079b00da5c8298ae77ab
Alexey Korchevskiy <mitallast@gmail.com>
2015-11-03 16:52:06 +0300

    improve provider mock test

--- provider/provider_test.go
+++ provider/provider_test.go
@@ -1,17 +1,49 @@
 package provider
 
-import "github.com/stretchr/testify/mock"
+import (
+	"github.com/stretchr/testify/mock"
+	"testing"
+	"log"
+)
 
-type mockProvider struct {
-    mock.Mock
+type MockProvider struct {
+	mock.Mock
 }
 
-func (p *mockProvider) Search(arg ...string) ([]byte, error) {
-    args := p.Called(arg)
-    return args.Get(0).([]byte), args.Error(1)
+func NewMockProvider() *MockProvider {
+	return &MockProvider{}
 }
 
-func (p *mockProvider) Install(arg ...string) ([]byte, error) {
-    args := p.Called(arg)
-    return args.Get(0).([]byte), args.Error(1)
+func (p *MockProvider) Search(arg ...string) ([]string, error) {
+	args := p.Called(arg)
+	return args.Get(0).([]string), args.Error(1)
+}
+
+func (p *MockProvider) Install(arg ...string) ([]string, error) {
+	args := p.Called(arg)
+	return args.Get(0).([]string), args.Error(1)
+}
+
+func TestSearch(t *testing.T) {
+	var provider *MockProvider
+	provider = NewMockProvider()
+	provider.Mock.On("Search", []string{"vim"}).Return([]string{}, nil)
+	lines, err := provider.Search("vim")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("lines: %s", lines)
+	}
+}
+
+func TestInstall(t *testing.T) {
+	var provider *MockProvider
+	provider = NewMockProvider()
+	provider.Mock.On("Install", []string{"vim"}).Return([]string{}, nil)
+	lines, err := provider.Install("vim")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("lines: %s", lines)
+	}
 }
